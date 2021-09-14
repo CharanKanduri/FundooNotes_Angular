d10d8457c4167c7a3f4be636c023a4175345ca43
Charan Kanduri <charan2000kanduri@gmail.com>
2021-09-14 15:58:36 +0530

    [CHARAN]Added: Register Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './Component/Register/register/register.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register',component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './Component/Register/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
+    
   ],
   imports: [
     BrowserModule,
