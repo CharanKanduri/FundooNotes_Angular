a2d0caa5fc340165445eb0f4c90c6bba477d700a
Charan Kanduri <charan2000kanduri@gmail.com>
2021-09-17 18:15:29 +0530

    [CHARAN]Added Forget Password Html and scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './Component/Register/register/register.component';
 import{LoginComponent} from './Component/Login/login/login.component';
+import {ForgetPasswordComponent} from './Component/Forgot-password/forget-password/forget-password.component';
 const routes: Routes = [
   {path:'register',component:RegisterComponent,
   },
   {path:'login',component:LoginComponent,
-  }
+  },
+  {path:'ForgetPassword',component:ForgetPasswordComponent}
+
 ];
 
 @NgModule({
