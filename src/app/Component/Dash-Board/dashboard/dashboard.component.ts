27d552bbac91973af7363fe002b9da8b943f52ed
Charan Kanduri <charan2000kanduri@gmail.com>
2021-09-22 16:18:20 +0530

    [CHARAN]Add: signout function

--- src/app/Component/Dash-Board/dashboard/dashboard.component.ts
+++ src/app/Component/Dash-Board/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   opened: boolean = true;
-  constructor() { }
+  constructor(private snackBar:MatSnackBar) { }
 
   ngOnInit(): void {
   }
-
+  signout()
+  {
+    localStorage.clear();
+    this.snackBar.open(``, 'Signout Successful', {
+      duration: 3000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'left'
+    });
+  }
 }
--- src/app/Component/add-note/add-note/add-note.component.ts
+++ src/app/Component/add-note/add-note/add-note.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import {MatSnackBar} from '@angular/material/snack-bar';
 
+
 @Component({
   selector: 'app-add-note',
   templateUrl: './add-note.component.html',
   styleUrls: ['./add-note.component.scss']
 })
 export class AddNoteComponent implements OnInit {
-  show:boolean = true;
-  noteColor = "white";
-  pin:boolean=false;
+  smallNote:boolean = true;
+  bigNote:boolean = false;
+  NoteForm !: FormGroup;
+  pinned = false;
+  noteColor = "#fff";
   isReminder=false;
   Reminder="";
-  NoteForm !: FormGroup;
   constructor(private snackBar:MatSnackBar) { }
 
   ngOnInit(): void {
@@ -22,12 +24,24 @@ export class AddNoteComponent implements OnInit {
       Description:new FormControl()
     });
   }
-  changePin(){
-    this.pin = !this.pin;
+  showNote()
+  {
+    this.smallNote = false;
+    this.bigNote = true;
+  }
+  pinNote()
+  {
+    
+      this.pinned=!this.pinned;
+    
   }
   RemoveReminder()
   {
     this.isReminder = false;
-    this.snackBar.open('Reminder Deleted', '', { duration: 3000,verticalPosition: 'bottom',horizontalPosition: 'left'});
+    this.snackBar.open('Reminder Deleted', '', {
+      duration: 2000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'left'
+    });
   }
 }
